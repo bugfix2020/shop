e97beefb9ae8db1bf0a904ae435125fd9ea17a90
liuyuxuan <670882978@qq.com>
2019-12-17 12:25:22 +0800

    update

---
+++ src/components/common/CpnMenu.vue
@@ -0,0 +1,40 @@
+<template>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <CpnMenuItem v-for="menu in menus" :key="menu.id" :item="menu"></CpnMenuItem>
+    </el-menu>
+</template>
+
+<script>
+    import {getMenuData} from "@/network/index";
+    import CpnMenuItem from "@/components/CpnMenuItem";
+
+    export default {
+        name: "CpnMenu",
+        data() {
+            return {
+                menus: [],
+                activeIndex: '1',
+            };
+        },
+        created() {
+            // eslint-disable-next-line no-console
+            console.log(1);
+            getMenuData().then(res => {
+                this.menus = res;
+            });
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                // eslint-disable-next-line no-console
+                console.log(key, keyPath);
+            }
+        },
+        components: {
+            CpnMenuItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-container>
+        <el-header>
+            <CpnMenu></CpnMenu>
+        </el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+    </el-container>
+</template>
+
+<script>
+    import CpnMenu from "@/components/common/CpnMenu";
+
+    export default {
+        name: "index",
+        data() {
+            return {
+                menus: [],
+            }
+        },
+        components:{
+            CpnMenu
+        },
+        created() {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
